e503172b8af02c4b7f81eebffc9a217e731320cc
Daniel Osuoha <122853068+DanielOsuoha@users.noreply.github.com>
2024-04-22 02:59:38 +0000

    made some minor changes

--- organize_files/organize.py
+++ organize_files/organize.py
@@ -1,15 +1,23 @@
 import os
 import shutil
 
+def organize_files(files, path):
+    for file in files:
+        if os.path.isfile(file):
+            # Get the file extension
+            filename, extension = os.path.splitext(file)
+            extension = extension[1:]  # Remove the dot from extension
+            
+            # Create directory if it doesn't exist
+            destination_dir = os.path.join(path, extension)
+            if not os.path.exists(destination_dir):
+                os.makedirs(destination_dir)
+            
+            # Move file to destination directory
+            shutil.move(file, os.path.join(destination_dir, os.path.basename(file)))
+
+# Example usage:
 path = input("Enter path: ")
 files = os.listdir(path)
-
-for file in files:
-    filename, extension = os.path.splitext(file)
-    extension = extension[1:]
-    
-    if os.path.exists(os.path.join(path, extension)):
-        shutil.move(file, os.path.join(path, extension))
-    else:
-        os.mkdir(os.path.join(path, extension))
-        shutil.move(file, os.path.join(path, extension))
+print(files)
+organize_files(files, path)
